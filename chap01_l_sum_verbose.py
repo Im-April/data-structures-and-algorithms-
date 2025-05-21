1c3167b7dfff7361564d2f88aa8129991275ca85
April <ghwon4640@gmail.com>
2025-05-21 20:28:21 +0900

    Add files

---
+++ chap01_l_sum_verbose.py
@@ -0,0 +1,20 @@
+# a로부터 b까지의 합 구하기
+
+print('a로부터 b까지의 정수의 합을 구하시오')
+a = int(input('정수 a를 입력하세요 : '))
+b = int(input('정수 b를 입력하세요 : '))
+
+if a > b :
+    a,b = b,a
+
+sum = 0
+for i in range(a,b+1):
+    if i < b:
+        print(f'{i} + ', end='')
+    else:
+        print(f'{i} = ', end='')
+
+    sum += i
+
+
+print(sum)
